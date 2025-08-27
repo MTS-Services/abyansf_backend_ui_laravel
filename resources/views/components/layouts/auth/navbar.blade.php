5520dde831263c860cd28950597f083e721be51d
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-08-27 08:46:07 +0600

    add

--- resources/views/components/layouts/auth/navbar.blade.php
+++ resources/views/components/layouts/auth/navbar.blade.php
@@ -46,14 +46,13 @@
          <a href="#" @click.prevent="activeTab = 'attendance'"
             :class="activeTab === 'attendance' ? 'mr-8 text-blue-600 font-bold border-b-2 border-blue-600' : 'mr-8'">
             Attendance
-        </a> 
+        </a>
     </div>
 </nav> --}}
 
 
 
-
-<<<<<<< HEAD
+ 
 <div class="flex items-center justify-center font-poppins">
     <!--
         Step 1: Make the main container responsive.
@@ -130,23 +129,10 @@
                         </button>
                     </div>
                 </div>
-=======
+
             <!-- Navigation links -->
             <!-- Use a flex container that wraps and centers items on smaller screens -->
-            <div
-                class="w-full flex justify-between flex-col sm:flex-row border rounded-sm items-center pl-14 pr-14 p-1 text-1xl font-bold text-gray-900  tracking-wider bg-[#e7e7e7]">
-                <a href="{{ route('admin.users') }}"
-                    class="text-gray-900 font-medium text-lg hover:text-blue-600 transition-colors duration-200 text-center navbar_a">Users</a>
-                <a href="{{ route('admin.bookings') }}"
-                    class="text-gray-900 font-medium text-lg hover:text-blue-600 transition-colors duration-200 text-center">Bookings</a>
-                <a href="#"
-                    class="text-gray-900 font-medium text-lg hover:text-blue-600 transition-colors duration-200 text-center">Listings</a>
-                <a href="#"
-                    class="text-gray-900 font-medium text-lg hover:text-blue-600 transition-colors duration-200 text-center">Event</a>
-                <a href="{{ route('admin.attendance') }}"
-                    class="text-gray-900 font-medium text-lg hover:text-blue-600 transition-colors duration-200 text-center">Attendance</a>
->>>>>>> 5e91c3fbbc36b2bebd5783d8fec0e9a1f7d11755
-            </div>
+
         </nav>
     </main>
 </div>
