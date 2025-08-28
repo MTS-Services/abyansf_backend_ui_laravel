f5453acc9e47a0ed5493eaf273f31dba76129f04
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-08-28 10:41:29 +0600

    add

--- resources/views/livewire/admin/user.blade.php
+++ resources/views/livewire/admin/user.blade.php
@@ -2,36 +2,36 @@
 
     <div class="#">
         <div class="w-[1200px] mx-auto p-4 mt-6">
-            <h2 class="text-2xl font-bold text-gray-800 mb-6">User Management</h2>
+            <h2 class=" font-medium text-3xl text-black mb-6">User Management</h2>
             <div class="overflow-hidden">
                 <!-- Table Container: Adds horizontal scroll for small devices if needed -->
                 <div class="overflow-x-auto">
                     <table class="min-w-full leading-normal hidden sm:table">
                         <thead>
-                            <tr class="bg-[#e7e7e7] text-gray-600 uppercase text-sm leading-normal font-bold">
-                                <th class="py-4 px-6 text-left">ID</th>
-                                <th class="py-4 px-6 text-left">Name</th>
-                                <th class="py-4 px-6 text-left">Email</th>
-                                <th class="py-4 px-6 text-left">Number</th>
-                                <th class="py-4 px-6 text-left">Join Date</th>
-                                <th class="py-4 px-6 text-left">Password</th>
-                                <th class="py-4 px-6 text-left">Status</th>
-                                <th class="py-4 px-6 text-left">Payment Link</th>
-                                <th class="py-4 px-6 text-center">Action</th>
+                            <tr class="bg-[#e7e7e7] text-black font-medium ">
+                                <th class="p-4 text-left font-medium text-base">ID</th>
+                                <th class="p-4 text-left font-medium text-base">Name</th>
+                                <th class="p-4 text-left font-medium text-base">Email</th>
+                                <th class="p-4 text-left font-medium text-base">Number</th>
+                                <th class="p-4 text-left font-medium text-base">Join Date</th>
+                                <th class="p-4 text-left font-medium text-base">Password</th>
+                                <th class="p-4 text-left font-medium text-base">Status</th>
+                                <th class="p-4 text-left font-medium text-base">Payment Link</th>
+                                <th class="p-4 text-center font-medium text-base">Action</th>
                             </tr>
                         </thead>
-                        <tbody class="text-gray-600 text-sm font-bold">
+                        <tbody class="text-balck text-sm ">
                             <!-- Desktop row 1 -->
-                            <tr class="hover:bg-gray-100">
-                                <td class="py-3 px-6 text-left whitespace-nowrap">01</td>
-                                <td class="py-3 px-6 text-left">John</td>
-                                <td class="py-3 px-6 text-left">john@gmail.com</td>
-                                <td class="py-3 px-6 text-left">+6842341354</td>
-                                <td class="py-3 px-6 text-left">01/05/2025</td>
-                                <td class="py-3 px-6 text-left">**********</td>
-                                <td class="py-3 px-6 text-left">Pending</td>
-                                <td class="py-3 px-6 text-left">
-                                    <a href="#" class="text-[#AD8945] hover:underline">Send</a>
+                            <tr class="#">
+                                <td class="p-4 text-left whitespace-nowrap font-normal">01</td>
+                                <td class="p-4 text-left font-normal text-base">John</td>
+                                <td class="p-4 text-left font-normal text-base">john@gmail.com</td>
+                                <td class="p-4 text-left font-normal text-base">+6842341354</td>
+                                <td class="p-4 text-left font-normal text-base">01/05/2025</td>
+                                <td class="p-4 text-left font-normal text-base">**********</td>
+                                <td class="p-4 text-left font-normal text-base">Pending</td>
+                                <td class="p-4 text-left font-normal text-base">
+                                    <a href="#" class="text-[#AD8945] ">Send</a>
                                 </td>
                                 <td class="py-3 px-6 text-center">
                                     <div class="flex items-center justify-center space-x-2">
@@ -75,18 +75,18 @@
                                 </td>
                             </tr>
                             <!-- Desktop row 2 -->
-                            <tr class=" border-gray-200 hover:bg-gray-100">
-                                <td class="py-3 px-6 text-left whitespace-nowrap">02</td>
-                                <td class="py-3 px-6 text-left">Jane</td>
-                                <td class="py-3 px-6 text-left">jane@gmail.com</td>
-                                <td class="py-3 px-6 text-left">+6842341354</td>
-                                <td class="py-3 px-6 text-left">01/05/2025</td>
-                                <td class="py-3 px-6 text-left">***********</td>
-                                <td class="py-3 px-6 text-left">Confirmed</td>
-                                <td class="py-3 px-6 text-left">
-                                    <a href="#" class="text-[#AD8945] hover:underline">Send</a>
+                            <tr class="pl-2">
+                                <td class="p-4 text-left whitespace-nowrap font-normal">01</td>
+                                <td class="p-4 text-left font-normal text-base">John</td>
+                                <td class="p-4 text-left font-normal text-base">john@gmail.com</td>
+                                <td class="p-4 text-left font-normal text-base">+6842341354</td>
+                                <td class="p-4 text-left font-normal text-base">01/05/2025</td>
+                                <td class="p-4 text-left font-normal text-base">**********</td>
+                                <td class="p-4 text-left font-normal text-base">Pending</td>
+                                <td class="p-4 text-left font-normal text-base">
+                                    <a href="#" class="text-[#AD8945] ">Send</a>
                                 </td>
-                                <td class="py-3 px-6 text-center">
+                                <td class="py-3 px-6 text-center pl-12">
                                     <div class="flex items-center justify-center space-x-2">
                                         <button class="text-[#AD8945] hover:text-blue-700" title="Edit">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
@@ -98,19 +98,20 @@
                                                     clip-rule="evenodd" />
                                             </svg>
                                         </button>
-                                        <button class="text-[#AD8945] hover:text-red-700" title="Delete">
+
+                                        <button class="text-[#AD8945]  hover:text-red-700" title="Delete">
 
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
-                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                class="size-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z">
-                                                </path>
+                                                    d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                             </svg>
 
                                         </button>
                                         <button class="text-[#AD8945] hover:text-red-700" title="Delete">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
-                                                fill="currentColor">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
+                                                viewBox="0 0 20 20" fill="currentColor">
                                                 <path fill-rule="evenodd"
                                                     d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm6 0a1 1 0 112 0v6a1 1 0 11-2 0V8z"
                                                     clip-rule="evenodd" />
